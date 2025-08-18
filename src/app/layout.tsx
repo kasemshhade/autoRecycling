d1aa5c44ba7a080acd5476cdb0b82a0fbb3ba5a2
kasemshhade <kasemshhade12@gmail.com>
2025-08-18 07:08:34 +0300

    Update layout.tsx refactor metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,33 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "KC Auto-Recycling",
-//   description: "webuycarsontario",
-// };
+export const metadata: Metadata = {
+  title: "Sell Your Used Car for Cash in Ontario, Canada | KC Auto-Recycling",
+  description:
+    "Get instant cash for your used car in Ontario, Canada with KC Auto-Recycling. We offer hassle-free transactions and top dollar for your vehicle. Contact us today!",
+  verification: {
+    google: "FHqc6NCHweS9fG-CcpFj9AA5PPHjwko0r-iXDaQKUK0",
+  },
+  alternates: {
+    canonical: process.env.NEXT_PUBLIC_SITE_URL,
+    languages: {
+      en: process.env.NEXT_PUBLIC_SITE_URL,
+      "x-default": process.env.NEXT_PUBLIC_SITE_URL,
+    },
+  },
+  keywords: [
+    "we buy cars Ontario",
+    "Sell used car Ontario",
+    "Canada",
+    "cash for cars",
+    "buy used cars",
+    "sell my car",
+    "instant cash",
+    "Ontario car buyers",
+  ],
+};
 
 export default function RootLayout({
   children,
@@ -16,39 +36,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <title>
-          Sell Your Used Car for Cash in Ontario, Canada | KC Auto-Recycling
-        </title>
-        <meta
-          name="description"
-          content="Get instant cash for your used car in Ontario, Canada with KC Auto-Recycling. We offer hassle-free transactions and top dollar for your vehicle. Contact us today!"
-          key="desc"
-        />
-        <meta
-          name="keywords"
-          content="we buy cars Ontario, Sell used car Ontario, Canada, cash for cars, buy used cars, sell my car, instant cash, Ontario car buyers"
-        />
-        <meta
-          name="google-site-verification"
-          content="FHqc6NCHweS9fG-CcpFj9AA5PPHjwko0r-iXDaQKUK0"
-        />
-        <link
-          rel="canonical"
-          href={process.env.NEXT_PUBLIC_SITE_URL}
-          key="canonical"
-        />
-        <link
-          rel="alternate"
-          href={process.env.NEXT_PUBLIC_SITE_URL}
-          hrefLang="en"
-        />
-        <link
-          rel="alternate"
-          href={process.env.NEXT_PUBLIC_SITE_URL}
-          hrefLang="x-default"
-        />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
